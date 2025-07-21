627ec35bb0e3234c8b5cf2adf544d1115c37dbc6
Aranna <melanieabalde0@gmail.com>
2025-07-21 18:19:22 +0800

    modal ui enhanced

--- src/pages/Personnel/core/checklist_modal.jsx
+++ src/pages/Personnel/core/checklist_modal.jsx
@@ -2206,7 +2206,7 @@ const ChecklistModal = ({
   >
     {isSubmitting ? (
       <>
-        <div className="animate-spin rounded-full h-5 w-5 border-2 border-white border-t-transparent"></div>
+        <div className="animate-spin rounded-full h-5 w-5 border-1 border-white border-t-transparent"></div>
         Submitting...
       </>
     ) : (
