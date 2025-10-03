e50ae5940764b8cdf551bd24cf9def5df328ec95
Chnix17 <vallechristianmark@gmail.com>
2025-10-04 01:58:24 +0800

    updated

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,7 +1,7 @@
 
-const API_BASE_URL = "http://localhost/gsd-reservation/backend/";
+// const API_BASE_URL = "http://localhost/gsd-reservation/backend/";
 
-// const API_BASE_URL = "https://peachpuff-alligator-715719.hostingersite.com/gsd/api/";
+const API_BASE_URL = "https://peachpuff-alligator-715719.hostingersite.com/gsd/api/";
 
 export const getApiBaseUrl = () => API_BASE_URL;
 
